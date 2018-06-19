4c15cfc67652292e0087e6a63c97fdb955bf277d
Somesh Singh <somesh.singh1992@gmail.com>
2018-06-19 12:40:18 +0200

    working with the drift element alone

---
+++ initial_test/sixtrack-v0/build/CMakeFiles/parallel_drift.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sosingh/sixtracklib_gsoc18/initial_test/sixtrack-v0/parallel_drift.cpp" "/home/sosingh/sixtracklib_gsoc18/initial_test/sixtrack-v0/build/CMakeFiles/parallel_drift.dir/parallel_drift.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "__NAMESPACE=st_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/sosingh/sixtracklib_gsoc18/studies/study8_martin/external/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ initial_test/sixtrack-v0/build/CMakeFiles/use_blocks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_blocks.dir/use_blocks.cpp.o"
+  "use_blocks.pdb"
+  "use_blocks"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/use_blocks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
