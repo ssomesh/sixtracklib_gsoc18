1dce0a292e4e44f07ea71d30c973ec1985c63bbd
Riccardo De Maria <riccardodemaria@gmail.com>
2018-06-20 23:32:11 +0200

    update

--- studies/study4/drift.h
+++ studies/study4/drift.h
@@ -7,8 +7,10 @@ TRACK_RETURN NS(Drift_track)(PARTICLES(p), _ELEMENT_MEM Drift_t *el){
     const REAL_T rpp = RPP(p);
     const REAL_T xp = PX(p) * rpp;
     const REAL_T yp = PY(p) * rpp;
+    TRACK_INIT
     X(p)+= xp * el->length;
     Y(p)+= yp * el->length;
     Z(p)+= e->length * (RVV - (1 + (SQ(xp) + SQ(yp)) / 2));
+    TRACK_EXIT
     return 0;
 };
